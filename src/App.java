c40c3371f34d31eb0e5ad4a3e18ffbe5f849e1b6
RuyanKumar <ruyankumar082@gmail.com>
2026-07-09 20:18:37 +0100

    app

---
+++ src/App.java
@@ -0,0 +1,84 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("testings");
+
+    byte age;
+    age = 100;
+    System.out.println(age);
+
+
+    short age2;
+    age2 = 5000;
+    System.out.println(age2);
+
+    int age3;
+    age3 = 100000;
+    System.out.println(age3);
+
+
+    long age4;
+    age4 = 10000000000l;
+    System.out.println(age4);
+
+
+    float height;
+    height = 5.75f;
+    System.out.println(height);
+
+
+    double height2;
+    height2 = 3.141592653589793d;
+    System.out.println(height2);
+
+
+    boolean happy;
+    happy = true;
+    System.out.println(happy);
+
+    boolean sick;
+    sick = false;
+    System.out.println(sick);
+
+    char grade;
+    grade = 'B';
+    System.out.println(grade);
+
+    String name;
+    name = "yes";
+    System.out.println(name);
+
+
+byte totalItems;
+totalItems = 50;
+System.out.println(totalItems);
+
+
+float unitCost;
+unitCost = 9.99f;
+System.out.println(unitCost);
+
+
+char dollarSign;
+dollarSign = '$';
+System.out.println(dollarSign);
+
+float totalCost;
+totalCost = unitCost*totalItems;
+System.out.println( "The total cost of the items = " + dollarSign + totalCost );
+
+
+
+var unitLost = 11.99;
+System.out.println(unitLost);
+
+
+var username = "no";
+System.out.println(username);
+
+var unitpow = 12; 
+unitpow = 13;
+unitpow = 12.3;
+System.out.println(unitpow);
+
+    }
+}
